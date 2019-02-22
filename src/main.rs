aa3e8995b37502d866d26b5e6e633750654f3c93
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-02-23 04:43:20 +0900

    exec command

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use rusoto_core::{
 use rusoto_iam::{Iam, IamClient};
 use rusoto_sts::{Credentials, GetSessionTokenRequest, Sts, StsClient};
 use std::collections::HashMap;
+use std::env;
 use std::process::{Command, Stdio};
 use std::sync::Arc;
 use tokio_core::reactor::Core;
@@ -33,23 +34,40 @@ fn main() -> Result<(), Error> {
     };
     info!("profile name: {}", profile_name);
 
+    info!("load from: {}", credentials_path.display());
     let mut credentials_ini = Ini::load_from_file(&credentials_path)?;
-    let expire = credentials_ini
+    let credentials = credentials_ini
         .section(Some(&profile_name as &str))
-        .and_then(|sec| sec.get("aws_expiration"))
-        .and_then(|expire| expire.parse::<DateTime<Utc>>().ok());
-    if let Some(ref expire) = expire {
-        info!("expiration: {}", expire);
+        .and_then(|sec| {
+            Some(Credentials {
+                access_key_id: sec.get("aws_access_key_id")?.to_owned(),
+                secret_access_key: sec.get("aws_secret_access_key")?.to_owned(),
+                session_token: sec.get("aws_session_token")?.to_owned(),
+                expiration: sec.get("aws_expiration")?.to_owned(),
+            })
+        });
+
+    if let Some(ref credentials) = credentials {
+        info!("expiration: {}", credentials.expiration);
     } else {
         info!("expiration: N/A");
     }
 
-    if expire.map_or(true, |expire| expire < Utc::now() + Duration::hours(3)) {
+    let credentials = if let Some(credentials) = credentials.filter(|credentials| {
+        credentials
+            .expiration
+            .parse::<DateTime<Utc>>()
+            .ok()
+            .map_or(false, |expiration| {
+                expiration >= Utc::now() + Duration::hours(3)
+            })
+    }) {
+        credentials
+    } else {
         let iam_client =
             IamClient::new_with(HttpClient::new()?, provider.clone(), Default::default());
         let sts_client =
             StsClient::new_with(HttpClient::new()?, provider.clone(), Default::default());
-
         let task = get_account_alias(&iam_client)
             .join3(get_caller_identity(&sts_client), get_yubikey_token_codes())
             .and_then(|(account_alias, (account, _, user_name), token_codes)| {
@@ -61,18 +79,40 @@ fn main() -> Result<(), Error> {
                     &token_codes,
                 )
             });
-
         let credentials = core.run(task)?;
+
         credentials_ini
             .with_section(Some(&profile_name as &str))
-            .set("aws_access_key_id", credentials.access_key_id)
-            .set("aws_secret_access_key", credentials.secret_access_key)
-            .set("aws_session_token", credentials.session_token)
-            .set("aws_expiration", credentials.expiration);
+            .set("aws_access_key_id", &credentials.access_key_id as &str)
+            .set(
+                "aws_secret_access_key",
+                &credentials.secret_access_key as &str,
+            )
+            .set("aws_session_token", &credentials.session_token as &str)
+            .set("aws_expiration", &credentials.expiration as &str);
+        info!("save to: {}", credentials_path.display());
         credentials_ini.write_to_file(&credentials_path)?;
-    }
+        credentials
+    };
 
-    Ok(())
+    let args: Vec<_> = env::args_os().collect();
+    if args.len() == 1 {
+        Ok(())
+    } else {
+        let status = Command::new(&args[1])
+            .args(&args[2..])
+            .env("AWS_ACCESS_KEY_ID", &credentials.access_key_id)
+            .env("AWS_SECRET_ACCESS_KEY", &credentials.secret_access_key)
+            .env("AWS_SESSION_TOKEN", &credentials.session_token)
+            .status()?;
+        if status.success() {
+            Ok(())
+        } else if let Some(code) = status.code() {
+            Err(format_err!("Command failed with exit code {}", code))
+        } else {
+            Err(format_err!("Command failed"))
+        }
+    }
 }
 
 fn get_account_alias<C>(client: &C) -> impl Future<Item = String, Error = Error>
